e73630ce799674b489c6a35193baf8878cd2ffed
Gramer <AlexeiDun@mail.ru>
2023-06-08 11:01:16 +0400

    Добавьте файлы проекта.

---
+++ Praktika/Cotrudnik/Chel.cs
@@ -0,0 +1,15 @@
+﻿namespace Praktika.Cotrudnik
+{
+    class Chel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Chel(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
+
---
+++ Praktika/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using Praktika.Admin;
+using Praktika.Cotrudnik;
+namespace Praktika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // user- администратор
+        private User user = new User("admin", "123");
+
+        // chel= сотрудник
+
+        private Chel chel = new Chel("cotrudnik", "456");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (user.UserName == tbUserName.Text && user.Password == pbPassword.Text)
+            {
+                Admin_Window admin_Window = new Admin_Window();
+                admin_Window.Show();
+
+
+
+            }
+            else
+
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+
+
+            if (chel.UserName == tbUserName.Text && chel.Password == pbPassword.Text)
+            {
+                Chel_Window chel_window = new Chel_Window();
+                chel_window.Show();
+            }
+            else
+
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+
+        }
+
+
+
+    }
+}    
+
